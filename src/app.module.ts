85f54c793679d3ee5b0fc82e0745d72c3f1b5e92
Muromi <devmuromi@gmail.com>
2023-01-27 10:33:35 +0900

    Add fk field to about contents-category, genre

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContentsModule } from './contents/contents.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ContentsModule],
+  imports: [
+    ContentsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'abc',
+      password: 'abc',
+      database: 'r-vod',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

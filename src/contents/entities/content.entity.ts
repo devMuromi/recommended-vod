076e55144f9baa9b29f97f0fde6c7e5d01ed33ff
dot0112 <dot0112@users.noreply.github.com>
2023-01-27 11:58:58 +0900

    add entitiy Platform VOD

--- src/contents/entities/content.entity.ts
+++ src/contents/entities/content.entity.ts
@@ -19,7 +19,10 @@ export class Content {
   category: Category;
 
   @ManyToOne((type) => Genre, (genre) => genre.contents)
-  genre: Category;
+  genre: Genre;
+
+  @OneToMany((type) => VOD, (vod) => vod.content)
+  vods: VOD[];
 
   @Column({ length: 500 })
   Image_URL: string;
@@ -47,4 +50,37 @@ export class Genre {
 
   @OneToMany((type) => Content, (content) => content.genre)
   contents: Content[];
+}
+
+@Entity()
+export class Platform {
+  @PrimaryGeneratedColumn()
+  Platform_id: number;
+
+  @Column({ length: 500 })
+  Name: string;
+
+  @OneToMany((type) => VOD, (VOD) => VOD.platform)
+  vods: VOD[];
+}
+
+@Entity()
+export class VOD {
+  @PrimaryGeneratedColumn()
+  VOD_id: number;
+
+  @Column({ length: 500 })
+  type: string;
+
+  @Column('int')
+  rent_period: number;
+
+  @ManyToOne((type) => Platform, (platform) => platform.vods)
+  platform: Platform;
+
+  @ManyToOne((type) => Content, (content) => content.vods)
+  content: Content;
+
+  @Column({ length: 500 })
+  vod_name: string;
 }

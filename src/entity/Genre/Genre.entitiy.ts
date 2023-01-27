e1b62c8d950bacdf4575c0158f10a35dc7279eaf
Muromi <devmuromi@gmail.com>
2023-01-27 10:39:16 +0900

    Style indent 2

--- src/entity/Genre/Genre.entitiy.ts
+++ src/entity/Genre/Genre.entitiy.ts
@@ -1,10 +1,1 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
-
-@Entity()
-export class Genre {
-    @PrimaryGeneratedColumn()
-    Genre_id: number;
-
-    @Column({ length: 500 })
-    Name: string;
-}

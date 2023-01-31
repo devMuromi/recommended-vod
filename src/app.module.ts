732b753016750f67d84781e608559b343d9ed3ef
dot0112 <dot0112@users.noreply.github.com>
2023-01-31 17:22:28 +0900

    add adminjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,10 @@ import { Module } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { AdminModule } from '@adminjs/nestjs';
+import * as AdminJSTypeorm from '@adminjs/typeorm';
+import AdminJS from 'adminjs';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContentsModule } from './contents/content.module';
@@ -23,6 +27,24 @@ import { ParserModule } from './parser/parser.module';
     ContentsModule,
     PlatformModule,
     ParserModule,
+    AdminModule.createAdminAsync({
+      useFactory: () => ({
+        adminJsOptions: {
+          rootPath: '/admin',
+          resources: [],
+        },
+        /*auth: {
+          authenticate,
+          cookieName: 'adminjs',
+          cookiePassword: 'secret',
+        },
+        sessionOptions: {
+          resave: true,
+          saveUninitialized: true,
+          secret: 'secret',
+        },*/
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
@@ -30,3 +52,18 @@ import { ParserModule } from './parser/parser.module';
 export class AppModule {
   constructor(private dataSource: DataSource) {}
 }
+
+/*const DEFAULT_ADMIN = {
+  email: 'admin@example.com',
+  password: 'password',
+};
+const authenticate = async (email: string, password: string) => {
+  if (email === DEFAULT_ADMIN.email && password === DEFAULT_ADMIN.password) {
+    return Promise.resolve(DEFAULT_ADMIN);
+  }
+  return null;
+};
+AdminJS.registerAdapter({
+  Resource: AdminJSTypeorm.Resource,
+  Database: AdminJSTypeorm.Database,
+})*/
